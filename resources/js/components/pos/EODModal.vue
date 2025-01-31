abb05b8d8062aa511972e8bd99bf4c7d60208090
ashaInfilogix <ashainfilogix@gmail.com>
2025-01-31 17:13:32 +0530

    change date format

--- resources/js/components/pos/EODModal.vue
+++ resources/js/components/pos/EODModal.vue
@@ -90,7 +90,7 @@ export default {
   },
   methods: {
     formatDate(date) {
-      return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+      return `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
     },
     async fetchSalesData() {
         const response = await axios.post('api/sales/today', { salesPersonId: this.salesPersonId });
