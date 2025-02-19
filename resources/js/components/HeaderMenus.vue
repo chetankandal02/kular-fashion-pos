27693795132fb2f9f69cc81e00bc77362df13a24
Satpal Singh Sekhon <satpalinfilogix@gmail.com>
2025-02-19 11:20:48 +0530

    Fixed responsive issue on navbar

--- resources/js/components/HeaderMenus.vue
+++ resources/js/components/HeaderMenus.vue
@@ -4,22 +4,22 @@
     <div class="dropdown dropdown-mega d-none d-lg-block ms-2">
         <button type="button" class="btn header-item waves-effect btn-success" data-bs-toggle="modal"
             data-bs-target="#searchArticalModal">
-            <span class="fs-5">Search Article</span>
+            <span>Search Article</span>
         </button>
 
         <button type="button" class="btn header-item waves-effect btn-dark" data-bs-toggle="modal"
             data-bs-target="#salesListModal">
-            <span class="fs-5">Sales List</span>
+            <span>Sales List</span>
         </button>
 
         <button type="button" class="btn header-item waves-effect btn-warning" data-bs-toggle="modal"
             data-bs-target="#onHoldSalesModal">
-            <span class="fs-5">On Hold Sales</span>
+            <span>On Hold Sales</span>
         </button>
 
         <button type="button" class="btn header-item waves-effect btn-secondary" data-bs-toggle="modal"
             data-bs-target="#transferInventoryModal">
-            <span class="fs-5">Transfer Inventory</span>
+            <span>Transfer Inventory</span>
         </button>
     </div>
 </template>
