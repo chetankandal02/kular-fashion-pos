bf480206f779438ed3a407ebff40c4472a0745fb
VishalInfilogix <vishalInfilogix@gmail.com>
2025-05-13 12:14:54 +0530

    chanage date format sale list module

--- resources/js/components/SalesListModal.vue
+++ resources/js/components/SalesListModal.vue
@@ -189,9 +189,15 @@ export default {
         },
 
         formatDateTime(timestamp) {
-            const date = new Date(timestamp);
-            return date.toLocaleString();
-        }
+    const date = new Date(timestamp);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}-${month}-${year} ${hours}:${minutes}`;
+}
     },
     mounted() {
         this.initializeDataTable();
