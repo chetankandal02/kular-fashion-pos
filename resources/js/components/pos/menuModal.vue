c7bb1baaeeab54b484b43fe40041c1c60b422b15
VishalInfilogix <vishalInfilogix@gmail.com>
2025-05-19 15:17:26 +0530

    display Inventory Transfer and Inventory Transfer History in the menu model and   fix image size in Search Article view

--- resources/js/components/pos/menuModal.vue
+++ resources/js/components/pos/menuModal.vue
@@ -7,35 +7,51 @@
                     <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="row">
+                    <div class="row  gx-2" >
                         <div class="col-md-6 mb-2">
-                            <button class="btn btn-dark btn-lg w-100" @click="printLastOrderReceipt">
+                            <button class="btn btn-dark btn-md w-100" @click="printLastOrderReceipt">
                                 <i class="mdi mdi-printer font-size-20 me-1"></i>
                                 Last Receipt
                             </button>
                         </div>
-                        <div class="col-md-6 mb-2">
-                            <button class="btn btn-secondary btn-lg w-100" @click="printLastGiftReceipt">
+                        <div class="col-md-6  mb-2">
+                            <button class="btn btn-secondary btn-md w-100" @click="printLastGiftReceipt">
                                 <i class="mdi mdi-printer font-size-20 me-1"></i>
                                 Gift Receipt
                             </button>
                         </div>
                         <div class="col-md-6 mb-2">
-                            <button class="btn btn-info btn-lg w-100" data-bs-toggle="modal" data-bs-target="#eodModal">
+                            <button class="btn btn-info btn-md w-100" data-bs-toggle="modal" data-bs-target="#eodModal">
                                 <i class="mdi mdi-cash-multiple font-size-20 me-1"></i>
                                 Running Total
                             </button>
                         </div>
                         <div class="col-md-6 mb-2">
-                            <button class="btn btn-success btn-lg w-100" data-bs-toggle="modal" data-bs-target="#eodModal">
+                            <button class="btn btn-success btn-md w-100" data-bs-toggle="modal"
+                                data-bs-target="#eodModal">
                                 <i class="mdi mdi-printer font-size-20 me-1"></i>
                                 EOD
                             </button>
                         </div>
                         <div class="col-md-6 mb-2">
-                            <button class="btn btn-primary btn-lg w-100" data-bs-toggle="modal" data-bs-target="#recallLayawayModal">
+                            <button class="btn btn-primary btn-md w-100" data-bs-toggle="modal"
+                                data-bs-target="#recallLayawayModal">
                                 <i class="mdi mdi-account-cash font-size-20 me-1"></i>
                                 Recall Layaway
+                            </button>
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <button class="btn btn-danger btn-md w-100" data-bs-toggle="modal"
+                                data-bs-target="#transferInventoryModal">
+                               <i class="mdi mdi-swap-horizontal font-size-20 me-1"></i> 
+                                Inventory Transfer
+                            </button>
+                        </div>
+                        <div class="col-md-6 mb-2 ">
+                            <button class="btn btn-warning btn-md  w-100 text-nowrap" data-bs-toggle="modal"
+                                data-bs-target="#transferInventoryHistoryModal">
+                               <i class="mdi mdi-transfer-right font-size-20 me-1"></i>
+                                Inventory Transfer History
                             </button>
                         </div>
                     </div>
@@ -62,7 +78,7 @@ export default {
         RecallLayawayModal
     },
     methods: {
-        async printLastOrderReceipt(){
+        async printLastOrderReceipt() {
             const response = await axios.post('/api/print-last-sale-receipt');
 
             if (response.data.success) {
@@ -76,7 +92,7 @@ export default {
                 alert(response.data.message || 'Something went wrong!')
             }
         },
-        async printLastGiftReceipt(){
+        async printLastGiftReceipt() {
             const response = await axios.post('/api/print-gift-receipt');
 
             if (response.data.success) {
